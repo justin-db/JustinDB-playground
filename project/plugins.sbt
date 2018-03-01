38a1af25d6e467d29813b53638c45053481579ae
speedcom <mateusz.maciaszekhpc@gmail.com>
2018-03-01 19:09:04 +0100

    runnable dockerized Key-Value cache cluster in Kubernetes cluster

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
+addSbtPlugin("com.typesafe.sbt"  % "sbt-native-packager"   % "1.2.2")
--- src/main/scala/db/justin/playground/Main.scala
+++ src/main/scala/db/justin/playground/Main.scala
@@ -2,81 +2,15 @@ package db.justin.playground
 
 import akka.actor.ActorSystem
 import akka.cluster.Cluster
-import akka.pattern.ask
-import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import db.justin.playground.ShoppingCart.LineItem
+import com.typesafe.scalalogging.StrictLogging
 
-import scala.concurrent.duration._
-import scala.util.Random
+object Main extends App with StrictLogging {
 
-object Main {
+  implicit val actorSystem = ActorSystem("ClusterSystem")
+  implicit val cluster = Cluster(actorSystem)
 
-  def main(args: Array[String]): Unit = {
-    val port = args(0)
+  actorSystem.actorOf(KeyValueCache.props)
 
-    val config = ConfigFactory
-      .parseString(s"""akka.remote.netty.tcp.port=$port""")
-      .withFallback(ConfigFactory.load())
-    implicit val actorSystem = ActorSystem("ClusterSystem", config)
-    implicit val cluster = Cluster(actorSystem)
-
-//    actorSystem.actorOf(Props[PingBot], "pingbot")
-//    actorSystem.actorOf(Props[EventSubscriberLogger], "EventSubscriberLogger")
-
-//    ------------
-//    implicit val timeout = Timeout(5.seconds)
-//    implicit val ec = actorSystem.dispatcher
-//
-//    val sc1 = actorSystem.actorOf(ShoppingCart.props("1"), "shopping-cart-1")
-//    val sc2 = actorSystem.actorOf(ShoppingCart.props("2"), "shopping-cart-2")
-//
-//    while (true) {
-//      Thread.sleep(1500)
-//
-//      // Get Cart
-//      (sc1 ? ShoppingCart.GetCart).mapTo[ShoppingCart.Cart].foreach { cart =>
-//        println("shopping-cart-1: " + cart.items)
-//      }
-//      (sc2 ? ShoppingCart.GetCart).mapTo[ShoppingCart.Cart].foreach { cart =>
-//        println("shopping-cart-2: " + cart.items)
-//      }
-//
-//      // Add or Remove
-//      if(Random.nextBoolean()) {
-//        println("Adding new elements")
-//        sc1 ! ShoppingCart.AddItem(LineItem(Random.nextInt(5).toString, "milk", 1))
-//        sc2 ! ShoppingCart.AddItem(LineItem(Random.nextInt(5).toString, "eggs", 10))
-//      } else {
-//        println("Possibly remove some elements")
-//        val times = Random.nextInt(2)
-//        (0 to times).foreach { _ =>
-//          sc1 ! ShoppingCart.RemoveItem(Random.nextInt(5).toString)
-//          sc2 ! ShoppingCart.RemoveItem(Random.nextInt(5).toString)
-//        }
-//      }
-//    }
-    //    ------------
-    val kvcache = actorSystem.actorOf(KeyValueCache.props)
-
-    implicit val timeout = Timeout(5.seconds)
-    implicit val ec = actorSystem.dispatcher
-
-    while(true) {
-      Thread.sleep(1000)
-      def randKey = Random.nextInt(4).toString
-
-      Random.nextInt(3) match {
-        case 0 =>
-          kvcache ! KeyValueCache.PutInCache(randKey, Random.nextString(10))
-        case 1 =>
-          kvcache ! KeyValueCache.Evict(randKey)
-        case 2 =>
-          (kvcache ? KeyValueCache.GetFromCache(randKey)).mapTo[KeyValueCache.Cached].foreach {
-            case KeyValueCache.Cached(key, Some(value)) => println(s"key: $key, value: $value")
-            case KeyValueCache.Cached(key, None) => println(s"key: $key, value: None")
-          }
-      }
-    }
-  }
+  logger.info("System has started working...")
 }
